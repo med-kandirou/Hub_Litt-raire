b7f17f38902488ebe2ab86bd4f68989f2619be9e
kandirou0 <simo.kandirou15@gmail.com>
2023-02-18 17:56:54 +0100

    update

--- front-end/src/views/admin/categories.vue
+++ front-end/src/views/admin/categories.vue
@@ -31,7 +31,7 @@
                                 {{ categorie.nom }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ categorie.created_at }}
+                                {{ categorie.created_at.slice(0,10) }}
                             </td>
                             <td class="px-6 py-4">
                                 <button @click="supprimerCat(categorie.id)" type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Supprimer</button>
