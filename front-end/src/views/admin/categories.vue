e079268e67c0d46fb6be7bc785f835e79c5701e7
kandirou0 <simo.kandirou15@gmail.com>
2023-02-18 13:23:34 +0100

    update

--- front-end/src/views/admin/categories.vue
+++ front-end/src/views/admin/categories.vue
@@ -18,24 +18,24 @@
                                 Date de creation
                             </th>
                             <th scope="col" class="px-6 py-3">
-                                
+                                Operations
                             </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="categori in categories" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <tr v-for="categorie in categories" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                             <td class="px-6 py-4">
-                                {{ categori.id }}
+                                {{ categorie.id }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ categori.nom }}
+                                {{ categorie.nom }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ categori.created_at }}
+                                {{ categorie.created_at }}
                             </td>
                             <td class="px-6 py-4">
-                                <button>delete</button>
-                                <button>modifier</button>
+                                <button @click="supprimerCat(categorie.id)" type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Supprimer</button>
+                                <button @click="update(categorie.id)" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Modifier</button>
                             </td>
                         </tr>
                     </tbody>
@@ -69,6 +69,24 @@
                 .then((res) =>{
                     this.categories=res.data;
             })
+        },
+        supprimerCat(id){
+            axios({
+                method: 'DELETE',
+                url: 'http://127.0.0.1:8000/api/admin/supprimerCat/'+id+'',
+                })
+                .then((res) =>{
+                    console.log(res.data);
+            })
+        },
+        modifierCat(id){
+            // axios({
+            //     method: 'DELETE',
+            //     url: 'http://127.0.0.1:8000/api/admin/supprimerCat/'+id+'',
+            //     })
+            //     .then((res) =>{
+            //         this.categories=res.data;
+            // })
         }
     },
     mounted(){
