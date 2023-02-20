fe57beda20774a95b0cc25fef9f8d6a4fd59b1d0
kandirou0 <simo.kandirou15@gmail.com>
2023-02-20 19:14:22 +0100

    update

--- front-end/src/views/admin/ajouter_livre.vue
+++ front-end/src/views/admin/ajouter_livre.vue
@@ -21,8 +21,10 @@
                 <div class="mb-6">
                     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
                     <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <option selected>Select category</option>
-                    <option value="US">United States</option>
+                        <option>Select category</option>
+                        <div v-for="cat in categories">
+                            <option :value="cat.id" >{{ cat.nom }}</option>
+                        </div>
                     </select>       
                 </div>
                 <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ajouter</button>
@@ -46,10 +48,25 @@
     components:{
         Sidebar
     },
-    data(){
-        return {
-            
-        }
-    },
-    }  
+    // data(){
+    //     return {
+    //         categories:''
+    //     }
+    // },
+    // methods:{
+    //     getCats(){
+    //         axios({
+    //             method: 'GET',
+    //             url: 'http://127.0.0.1:8000/api/admin/getCats',
+    //             })
+    //             .then((res) =>{
+    //                 this.categories=res.data;
+    //         })
+    //     },
+    // },
+    // mounted(){
+    //     this.getCats();
+    // }
+    
+}  
 </script>
