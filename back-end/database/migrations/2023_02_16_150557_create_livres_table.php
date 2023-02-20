5dfea9f3c36b3e33eb47ac82b9b38786e5db2bf5
kandirou0 <simo.kandirou15@gmail.com>
2023-02-20 17:05:50 +0100

    update

--- back-end/database/migrations/2023_02_16_150557_create_livres_table.php
+++ back-end/database/migrations/2023_02_16_150557_create_livres_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('livres', function (Blueprint $table) {
             $table->id();
             $table->string('nom');
+            $table->text('image');
             $table->date('date');
             $table->foreignId('id_cat')->references('id')->on('categories')->onDelete('cascade')->onUpdate('cascade');
         });
--- front-end/src/views/admin/ajouter_livre.vue
+++ front-end/src/views/admin/ajouter_livre.vue
@@ -3,6 +3,25 @@
         <Sidebar class="sm:h-screen bg-gray-900 fixed"/>
         <div class="sm:col-start-2 sm:col-span-3 mx-auto">
             <h1 class="mt-10 mb-9 text-center text-4xl font-extrabold tracking-tight leading-none text-gray-900">Ajouter un livre</h1>
+
+            
+            <form>
+                <div class="mb-6">
+                    <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom de livre</label>
+                    <input type="text" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Nom" >
+                </div>
+                <div class="mb-6">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image de livre</label>
+                    <input type="file" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" >
+                </div>
+                <div class="mb-6">
+                    <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contenu de livre</label>
+                    <input type="file" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" >
+                </div>
+                
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
+            </form>
+
             
             
 
@@ -11,3 +30,20 @@
     
 
 </template>
+
+
+<script >
+    import axios from 'axios'
+    import Sidebar from '@/components/sidebar.vue'
+    export default {
+    name:'ajouter_livre',
+    components:{
+        Sidebar
+    },
+    data(){
+        return {
+            
+        }
+    },
+    }  
+</script>
