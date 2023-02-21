097110c97cc749abd7beb10363d8b86a301812d3
kandirou0 <simo.kandirou15@gmail.com>
2023-02-21 14:11:55 +0100

    update

--- front-end/src/views/user/livres.vue
+++ front-end/src/views/user/livres.vue
@@ -4,7 +4,7 @@
 
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
         <div v-for="livre in livres">
-            <Livre :id_livre="livre.id" :image="livre.image" :nom="livre.nom_livre" :file="livre.pdf" :cat="livre.nom_cat" :date="livre.created_at"  />
+            <Livre @ajouterFavorie="ajouterFavorie" :id_livre="livre.id" :image="livre.image" :nom="livre.nom_livre" :file="livre.pdf" :cat="livre.nom_cat" :date="livre.created_at"  />
         </div>      
     </div>
 
@@ -13,6 +13,7 @@
 
 
 <script >
+    import Cookies from 'vue-cookies'
     import axios from 'axios'
     import Livre from '@/components/livre.vue'
     import Header from '@/components/header_user.vue'
@@ -37,6 +38,19 @@ export default{
                     this.livres=res.data;
             })
         },
+        ajouterFavorie(id){
+            axios({
+                method: 'POST',
+                url: 'http://127.0.0.1:8000/api/user/ajouterFavorie',
+                data:{
+                    id_livre:id,
+                    id_user:Cookies.get('id')
+                }
+                })
+                .then((res) =>{
+                    this.livres=res.data;
+            })
+        }
     },
     mounted(){
         this.getLivres();
