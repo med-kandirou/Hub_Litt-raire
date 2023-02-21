e2a5a89b8ffccff4123daa7e0b002f7b414b95cc
kandirou0 <simo.kandirou15@gmail.com>
2023-02-21 11:57:43 +0100

    update

--- front-end/src/views/user/livres.vue
+++ front-end/src/views/user/livres.vue
@@ -2,6 +2,11 @@
     <Header />
     <h1 class="mt-10 mb-9 text-center text-4xl font-extrabold tracking-tight leading-none text-gray-900">Les livres</h1>
 
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+        <div v-for="reservation in reservations">
+            
+        </div>      
+    </div>
 
 </template>
 
