a21e24a1050ba03fb44368b1b89a21dc8013c467
kandirou0 <simo.kandirou15@gmail.com>
2023-02-21 21:12:08 +0100

    update

--- front-end/src/views/user/favories.vue
+++ front-end/src/views/user/favories.vue
@@ -3,7 +3,7 @@
     <h1 class="mt-10 mb-9 text-center text-4xl font-extrabold tracking-tight leading-none text-gray-900">Mes favories</h1>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
         <div v-for="favorie in favories">
-            <Livre page="favorie" @supprimerfavorie="supprimerfavorie" :id_livre="favories.id" :image="favories.image" :nom="favories.nom_livre" :file="favories.pdf" :cat="favories.nom_cat" :date="favories.created_at"  />
+            <Livre page="favorie" @supprimerfavorie="supprimerfavorie" :id_livre="favorie.id" :image="favorie.image" :nom="favorie.nom_livre" :file="favorie.pdf" :cat="favorie.nom_cat" :date="favorie.created_at"  />
         </div>      
     </div>
 
@@ -11,8 +11,10 @@
 
 
 <script >
-import Cookies from 'vue-cookies'
-import Header from '@/components/header_user.vue'
+    import axios from 'axios'
+    import Cookies from 'vue-cookies'
+    import Header from '@/components/header_user.vue'
+    import Livre from '@/components/livre.vue'
 export default{
     name:'favories_comp',
     data(){
@@ -21,7 +23,8 @@ export default{
         }
     },
     components:{
-        Header
+        Header,
+        Livre
     },
     methods:{
         getLivres(){
@@ -33,6 +36,9 @@ export default{
                     this.favories=res.data;
             })
         },
+        supprimerfavorie(){
+
+        }
     },
     mounted(){
         this.getLivres();
