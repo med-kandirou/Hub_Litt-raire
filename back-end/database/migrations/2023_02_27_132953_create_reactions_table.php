696195b6bf06a4a4279248bf3e11cc81c58cf83a
kandirou0 <simo.kandirou15@gmail.com>
2023-02-27 14:30:14 +0100

    add reaction table

--- back-end/database/migrations/2023_02_27_132953_create_reactions_table.php
+++ back-end/database/migrations/2023_02_27_132953_create_reactions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('likes', function (Blueprint $table) {
+        Schema::create('reactions', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('likes');
+        Schema::dropIfExists('reactions');
     }
 };
