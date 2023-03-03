8fa152b6f13dd8297fecc3899f9c041fcf05d5db
kandirou0 <simo.kandirou15@gmail.com>
2023-03-03 15:44:23 +0100

    update

--- front-end/src/views/admin/livres.vue
+++ front-end/src/views/admin/livres.vue
@@ -52,15 +52,15 @@
                             <form class="space-y-6">
                                 <div>
                                     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom de categorie</label>
-                                    <input type="text" v-model="nom" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nom de categorie" >
+                                    <input type="text" v-model="nom" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nom de categorie" >
                                 </div>
                                 <div>
                                     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image</label>
-                                    <img src="#" />
+                                    <input type="file" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" >
                                 </div>
                                 <div>
                                     <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contenu</label>
-                                    <img src="#" />
+                                    <input type="file" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" >
                                 </div>
                                 <div>
                                     <select >
@@ -68,7 +68,7 @@
                                         <option value="">Option 2</option>
                                     </select>
                                 </div>
-                                <button  type="button" @click="modifierCat" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Modifier</button>
+                                <button  type="button" @click="modifierBook" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Modifier</button>
                             </form>
                         </div>
                         </div>
@@ -96,7 +96,12 @@
     },
     data(){
         return {
-            livres:null
+            livres:null,
+            livre:{
+                id:null,
+                nom:null
+            }
+            
         }
     },
     methods:{
@@ -121,7 +126,14 @@
             })
         },
         toggle(id){
-            
+            axios({
+                method: 'get',
+                url: 'http://127.0.0.1:8000/api/livre/getlivre/'+id+'',})
+                .then((res) =>{
+                    this.livre.nom=res.data.nom;
+                    this.livre.id=res.data.id;
+            });
+
             var modal = document.getElementById("myModal");
             var span = document.getElementsByClassName("close")[0];
             modal.style.display = "block";
