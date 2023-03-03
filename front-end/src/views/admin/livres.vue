463828b4ff8bb9287ce7bcf87404e7a56968147c
kandirou0 <simo.kandirou15@gmail.com>
2023-03-03 17:11:38 +0100

    update

--- front-end/src/views/admin/livres.vue
+++ front-end/src/views/admin/livres.vue
@@ -52,7 +52,7 @@
                             <form class="space-y-6">
                                 <div>
                                     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom de categorie</label>
-                                    <input type="text" v-model="nom" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nom de categorie" >
+                                    <input type="text" v-model="livre.nom" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nom de categorie" >
                                 </div>
                                 <div>
                                     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image</label>
@@ -63,10 +63,9 @@
                                     <input type="file" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" >
                                 </div>
                                 <div>
-                                    <select >
-                                        <option value="">Option 1</option>
-                                        <option value="">Option 2</option>
-                                    </select>
+                                    <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                        <option v-for="cat in categories" :value="cat.id" >{{ cat.nom }}</option>
+                                    </select> 
                                 </div>
                                 <button  type="button" @click="modifierBook" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Modifier</button>
                             </form>
@@ -100,8 +99,8 @@
             livre:{
                 id:null,
                 nom:null
-            }
-            
+            },
+            categories:''
         }
     },
     methods:{
@@ -125,6 +124,14 @@
                 }  
             })
         },
+        getcate(){
+            axios({
+                method: 'get',
+                url: 'http://127.0.0.1:8000/api/admin/getCats',})
+                .then((res) =>{
+                    this.categories=res.data;
+            });
+        },
         toggle(id){
             axios({
                 method: 'get',
@@ -133,6 +140,7 @@
                     this.livre.nom=res.data.nom;
                     this.livre.id=res.data.id;
             });
+            this.getcate();
 
             var modal = document.getElementById("myModal");
             var span = document.getElementsByClassName("close")[0];
