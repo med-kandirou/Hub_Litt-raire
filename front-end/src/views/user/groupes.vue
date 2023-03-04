fb2906593ad7ead2038f49b87210acee757a189d
kandirou0 <simo.kandirou15@gmail.com>
2023-03-04 17:57:22 +0100

    update

--- front-end/src/views/user/groupes.vue
+++ front-end/src/views/user/groupes.vue
@@ -42,7 +42,7 @@
                                 <button @click="rejoindreGroup(group.id)" type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Rejoindre</button>
                             </td>
                             <td v-else class="px-6 py-4">
-                                <button @click="discuter(group.id)" type="button" class="text-white bg-gradient-to-r from-blue-800 via-blue-800 to-blue-800 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-800 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Discuter</button>
+                                <button  @click="discuter(group.id)" type="button" class="text-white bg-gradient-to-r from-blue-800 via-blue-800 to-blue-800 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-800 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Discuter</button>
                             </td>
                         </tr>
                     </tbody>
@@ -95,6 +95,9 @@ export default{
         Header
     },
     methods:{
+        discuter(id){
+            this.$router.push('/user/groupe/'+id);
+        },
         getGroups(){
             axios({
                 method: 'GET',
