5316f8faa425507d547ac65a7d254c374299dc23
kandirou0 <simo.kandirou15@gmail.com>
2023-03-04 17:47:43 +0100

    update

--- front-end/src/views/user/groupes.vue
+++ front-end/src/views/user/groupes.vue
@@ -4,7 +4,7 @@
     
     
     <div class="">
-        <center><button @click="toggle" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Creer votre group</button></center><br>
+        <center><button @click="toggle" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Creer votre groupe</button></center><br>
 
 
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
