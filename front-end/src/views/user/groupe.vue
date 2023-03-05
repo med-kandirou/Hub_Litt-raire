0acf6215a6a13714e07c4bf6bf5a35908643969d
kandirou0 <simo.kandirou15@gmail.com>
2023-03-05 16:18:53 +0100

    update

--- front-end/src/views/user/groupe.vue
+++ front-end/src/views/user/groupe.vue
@@ -18,7 +18,7 @@
             </div>
         </div>
         <div class="w-1/2">
-            <h1 class="text-center text-2xl font-extrabold tracking-tight leading-none text-gray-900">Messages</h1>
+            <h1 class="text-center text-2xl mb-2 font-extrabold tracking-tight leading-none text-gray-900">Messages</h1>
             <div class="relative">
                 <input type="text" v-model="message" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Envoyer un message" >
                 <button type="button" @click="send" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Envoyer</button>
