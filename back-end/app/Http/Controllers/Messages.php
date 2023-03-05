d3402be0f891c18d90afd32a559c64c4928facf7
kandirou0 <simo.kandirou15@gmail.com>
2023-03-05 17:51:19 +0100

    updqte

--- back-end/app/Http/Controllers/Messages.php
+++ back-end/app/Http/Controllers/Messages.php
@@ -16,7 +16,7 @@ class Messages extends Controller
     }
 
     public function getmessages($id){
-        Message::select('messages.message','users.nom','users.prenom')
+        return Message::select('messages.message','users.nom','users.prenom')
         ->join('users','users.id','messages.id_user')
         ->where('messages.id_group','=',$id)
         ->get();
