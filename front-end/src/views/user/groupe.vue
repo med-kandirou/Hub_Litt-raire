8fda8c5b52bf84c49829c0268d67db0e760b1b08
kandirou0 <simo.kandirou15@gmail.com>
2023-03-05 13:24:36 +0100

    update

--- front-end/src/views/user/groupe.vue
+++ front-end/src/views/user/groupe.vue
@@ -1,3 +1,11 @@
 <template>
-    <h1>groupe </h1>
+    <Header />
+    <h1 class="mt-10 mb-9 text-center text-4xl font-extrabold tracking-tight leading-none text-gray-900">Discuter dans le groupe</h1>
 </template>
+
+
+<script setup>
+    import Header from '@/components/header_user.vue'
+
+    
+</script>
