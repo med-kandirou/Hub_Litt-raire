9eea5a30d649c11ecf4d02a1fa1906317ab0f12a
kandirou0 <simo.kandirou15@gmail.com>
2023-03-06 14:17:20 +0100

    update

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    host:true,
+    port:3000
+  },
   resolve:{
     alias:{
       '@' : path.resolve(__dirname, './src'),
